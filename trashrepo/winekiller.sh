a9e7ad34fb2c846adfac3ce1860e7509d878772a
wesipls <43371084+wesipls@users.noreply.github.com>
2019-02-06 16:35:50 +0200

    Update and rename willer.sh to winekiller.sh
    added description of killed instances

--- trashrepo/winekiller.sh
+++ trashrepo/winekiller.sh
@@ -6,13 +6,19 @@ counter=0
 
 supervar=()
 
+#Find running instances
+
 while IFS= read -r line; do
     supervar+=( "$line" )
 done < <( ps aux | grep -v -E "grep|awk|.sh" | grep wine | awk '{print $2}')
 
 #Prints out the found instances
-echo ${supervar[*]}
 
+echo ""
+echo "$(ps aux | grep -v -E "grep|awk|.sh" | grep wine | awk '{print $2, $11, $12}')"
+echo ""
+
+#Kill running wine instances
 
 for i in "${supervar[@]}"; do
 		if [[ $i != $$ ]]; then
@@ -22,3 +28,4 @@ for i in "${supervar[@]}"; do
 done
 
 echo $counter processes killed
+echo ""
