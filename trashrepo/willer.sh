0c5c1c8d7aeea04ba713342feac4047d93df3294
wesipls <43371084+wesipls@users.noreply.github.com>
2019-02-06 16:11:16 +0200

    Update willer.sh
    Removed chance to kill random PIDs

--- trashrepo/willer.sh
+++ trashrepo/willer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Finds and kills all wine instances running.
-#WARNING: Part code, part spaghetti, there is a 1% chance some other PID is killed if started with the PID used by this scripts grep or awk, althought this is a 1-2 second window where this could happend, so dont worry ;)
 
 counter=0
 
@@ -9,15 +8,17 @@ supervar=()
 
 while IFS= read -r line; do
     supervar+=( "$line" )
-done < <( ps aux | grep wine | awk '{print $2}' )
+done < <( ps aux | grep -v -E "grep|awk|.sh" | grep wine | awk '{print $2}')
+
+#Prints out the found instances
+echo ${supervar[*]}
+
 
 for i in "${supervar[@]}"; do
-	if [ "$(ps -p $i | tail -n 1 | awk '{print $4}')" != CMD ]; then
 		if [[ $i != $$ ]]; then
 		kill -9 "$i"
 		counter=$((counter+1))
 		fi
-	fi
 done
 
 echo $counter processes killed
