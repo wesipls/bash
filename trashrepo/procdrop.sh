24f22c6b7645fb27352193735ddf9f444311340a
wesipls <43371084+wesipls@users.noreply.github.com>
2019-02-07 12:46:27 +0200

    Update and rename winekiller.sh to procdrop.sh
    Updated script to work with any running processes.
    
    Usage: bash procdrop.sh $1
    Where $1 is the keyword you want to stop processes for.
    
    Example: procdrop.sh wine will drop all running processes including the word wine

--- trashrepo/procdrop.sh
+++ trashrepo/procdrop.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#Finds and kills all wine instances running.
+#Finds and kills all instances running by keyword
 
 counter=0
-
 supervar=()
 
 #Find running instances
 
 while IFS= read -r line; do
     supervar+=( "$line" )
-done < <( ps aux | grep -v -E "grep|awk|.sh" | grep wine | awk '{print $2}')
+done < <( ps aux | grep -v -E "grep|awk|procdrop" | grep $1 | awk '{print $2}')
 
 #Prints out the found instances
 
 echo ""
-echo "$(ps aux | grep -v -E "grep|awk|.sh" | grep wine | awk '{print $2, $11, $12}')"
+echo "$(ps aux | grep -v -E "grep|awk|procdrop" | grep $1 | awk '{print $2, $11}')"
 echo ""
 
-#Kill running wine instances
+#Kill running instances
 
 for i in "${supervar[@]}"; do
 		if [[ $i != $$ ]]; then
@@ -26,6 +25,8 @@ for i in "${supervar[@]}"; do
 		counter=$((counter+1))
 		fi
 done
+
+
 
 echo $counter processes killed
 echo ""
