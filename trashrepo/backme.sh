7d93685517f5fecfd6343e1b9bf856742a297a57
wesipls <43371084+wesipls@users.noreply.github.com>
2019-05-10 14:25:56 +0300

    Update backme.sh
    Added logging and better synthax.
    TODO: remove "/" from end of string if $1 is a folder and "/" is included in the name

--- trashrepo/backme.sh
+++ trashrepo/backme.sh
@@ -1,75 +1,115 @@
 #!/bin/bash
 #Shell script to backup and organize messy users development folders
-#All files are backed up to /home/$USER/BACKME, non-directory projects are saved into a folder with the (language)-projects directory
-#Example, python projects will be saved to py-projects, javascript will be saved to js-projects
 #$1 is the name of the file or folder
 #THIS SCRIPT DOES NOT WORK WITH HIDDEN FOLDERS RIGHT NOW, DUE TO '.' DELIMETER BEING USED FOR CUTTING VARIABLES
-#TODO: add file compression
 
-
+#if [ echo "$1" | grep  ]
 #Check and create backup root folder
+
 if [ -d "/home/$USER/BACKME" ] ; then
 	echo""
-	echo "backme root exists, starting backup..."
+	echo "[OK] backme root exists, starting backup..."
 else 
 	echo""
-	echo "backme root not found, creating root..."
+	echo "[INFO] backme root not found, creating root..."
 	mkdir "/home/$USER/BACKME"
 fi
+
+touch "/home/$USER/BACKME/log"
+
+if [ "$(cat /home/$USER/BACKME/log | grep $(date "+%y/%m/%d"))" ] ; then
+	:
+else
+	echo "|#################################|" >> /home/$USER/BACKME/log
+	echo "|        $(date "+%y/%m/%d %H:%M:%S")        |" >> /home/$USER/BACKME/log
+	echo "|#################################|" >> /home/$USER/BACKME/log
+fi
+
+
+#THIS WILL PROLLY BREAK SOMETHING (TODO: REMOVE "/" FROM END OF STRING IF INCLUDED IN FOLDER NAME
+#if [[ $1 == */ ]] ; then
 
 k=$(echo "$1" | rev | cut -d '/' -f 1 | rev)
 
 #Check if $1 is file or folder, and continue accordingly
-if [ -d "$1" ] ; then #IF $1 IS FOLDER, TODO: CREATE BACKUPING
+if [ -d "$1" ] ; then
 	echo ""
-	echo "$1 is a folder, backing up into /BACKME/$k..."
+	echo "[OK] $1 is a folder, backing up into /BACKME/$k..."
 	if [ -d "/home/$USER/BACKME/$k" ] ; then
 		echo""
-		echo" Folder $k already exists, using available folder"
+		echo "[OK] Folder $k already exists, using available folder"
 		cp -r $1 /home/$USER/BACKME/$k/${k}_$(date "+%y%m%d%H%M%S")
+		echo "|" >> /home/$USER/BACKME/log
+		echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACKED UP] FOLDER ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $k" >> /home/$USER/BACKME/log
+		echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 	else
 		echo""
-		echo "Folder $k does not exist yet, creating folder"
+		echo "[INFO] Folder $k does not exist yet, creating folder"
 		mkdir "/home/$USER/BACKME/$k"
 		cp -r $1 /home/$USER/BACKME/$k/${k}_$(date "+%y%m%d%H%M%S")
+		echo "|" >> /home/$USER/BACKME/log
+		echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACKED UP] FOLDER ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $k" >> /home/$USER/BACKME/log
+		echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORGINAL] $1 was backed up" >> /home/$USER/BACKME/log
+
 	fi
 elif [ -f "$1" ] ; then
 	y=$(echo "$1" | rev | cut -d '.' -f 1 | rev)
 	echo""
-	echo "$1 is a file, backing up, saving in to $y-projects..."
+	echo "[OK] $1 is a file, backing up, saving in to $y-projects..."
 	x=$(echo "$k" | cut -d '.' -f 1)
 	if [ -d "/home/$USER/BACKME/$y-projects" ] ; then
 		echo""
-		echo "$y-projects already exists, digging down..."
+		echo "[OK] $y-projects already exists, digging down..."
 			if [ -d "/home/$USER/BACKME/$y-projects/$x" ] ; then
 				echo""
-				echo "Folder $x in $y-projects already exists, making new entry..."
+				echo "[OK] Folder $x in $y-projects already exists, making new entry..."
 				cp $1 /home/$USER/BACKME/$y-projects/$x/${k}_$(date "+%y%m%d%H%M%S")
+				echo "|" >> /home/$USER/BACKME/log
+				echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACK IN] FILE ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $y-projects" >> /home/$USER/BACKME/log
+				echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 			else
 				mkdir "/home/$USER/BACKME/$y-projects/$x"
 				echo""
-				echo "Folder $x in $y-projects has been created, making new entry..."
+				echo "[INFO] Folder $x in $y-projects has been created, making new entry..."
 				cp $1 /home/$USER/BACKME/$y-projects/$x/${k}_$(date "+%y%m%d%H%M%S")
+				echo "|" >> /home/$USER/BACKME/log
+				echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACK IN] FILE ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $y-projects" >> /home/$USER/BACKME/log
+				echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 			fi
 	else
 		echo""
-		echo "$y-projects folder missing, creating folder..."
+		echo "[INFO] $y-projects folder missing, creating folder..."
 		mkdir "/home/$USER/BACKME/$y-projects"
 			if [ -d "/home/$USER/BACKME/$y-projects/$x" ] ; then
 				echo""
-				echo "Folder $x in $y-projects already exists, making new entry..."
+				echo "[OK} Folder $x in $y-projects already exists, making new entry..."
 				cp $1 /home/$USER/BACKME/$y-projects/$x/${k}_$(date "+%y%m%d%H%M%S")
+				echo "|" >> /home/$USER/BACKME/log
+				echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACK IN] FILE ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $y-projects" >> /home/$USER/BACKME/log
+				echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORIGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 			else
 				mkdir "/home/$USER/BACKME/$y-projects/$x"
 				echo ""
-				echo "Folder $x in $y-projects has been created, making new entry..."
+				echo "[INFO] Folder $x in $y-projects has been created, making new entry..."
 				cp $1 /home/$USER/BACKME/$y-projects/$x/${k}_$(date "+%y%m%d%H%M%S")
+				echo "|" >> /home/$USER/BACKME/log
+				echo "|___$(date "+%y/%m/%d %H:%M:%S")    [BACK IN] FILE ${k}_$(date "+%y%m%d%H%M%S") has been saved to the folder $y-projects" >> /home/$USER/BACKME/log
+				echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORIGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 			fi
 	fi
 		
 
 else
 	echo""
-	echo "!!! Something went wrong, nothing to backup !!!"
+	echo "[ERROR] !!! Something went wrong, nothing to backup !!!"
+	echo "|" >> /home/$USER/BACKME/log
+	echo "|___$(date "+%y/%m/%d %H:%M:%S")    [ERROR] Something funky happened, nothing was backed up                                                 [ERROR]" >> /home/$USER/BACKME/log
 fi
+d=$(du -sh /home/kim/BACKME/ | awk -F'\t' '{print $1}')
+echo "|_________$(date "+%y/%m/%d %H:%M:%S")    [DISKSPACE] The BACKME/ folder is currently $d in size" >> /home/$USER/BACKME/log
+echo""
+echo "[CURRENTLY USED DISKPACE]"
+echo "$(du -hs /home/$USER/BACKME/* | awk -F'\t' '{print $1 " " $2;}')"
+echo""
+echo "[END] All done captain!"
 echo""
