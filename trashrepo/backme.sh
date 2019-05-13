39982750409ba3bbf9c4af30fe958e0301433259
wesipls <43371084+wesipls@users.noreply.github.com>
2019-05-13 12:20:26 +0300

    Update backme.sh

--- trashrepo/backme.sh
+++ trashrepo/backme.sh
@@ -4,7 +4,6 @@
 #THIS SCRIPT DOES NOT WORK WITH HIDDEN FOLDERS RIGHT NOW, DUE TO '.' DELIMETER BEING USED FOR CUTTING VARIABLES
 
 #Check and create backup root folder
-
 if [ -d "/home/$USER/BACKME" ] ; then
 	echo""
 	echo "[OK] backme root exists, starting backup..."
@@ -14,8 +13,8 @@ else
 	mkdir "/home/$USER/BACKME"
 fi
 
+#Create new log entry for today if none available
 touch "/home/$USER/BACKME/log"
-
 if [ "$(cat /home/$USER/BACKME/log | grep $(date "+%y/%m/%d"))" ] ; then
 	:
 else
@@ -24,13 +23,9 @@ else
 	echo "|#################################|" >> /home/$USER/BACKME/log
 fi
 
-
-#THIS WILL PROLLY BREAK SOMETHING (TODO: REMOVE "/" FROM END OF STRING IF INCLUDED IN FOLDER NAME
-#if [[ $1 == */ ]] ; then
-
-k=$(echo "$1" | rev | cut -d '/' -f 1 | rev)
-
-#Check if $1 is file or folder, and continue accordingly
+#Check if $1 is a file or folder and save to correct location
+##If folder
+k=$(basename $1)
 if [ -d "$1" ] ; then
 	echo ""
 	echo "[OK] $1 is a folder, backing up into /BACKME/$k..."
@@ -51,6 +46,7 @@ if [ -d "$1" ] ; then
 		echo "|______$(date "+%y/%m/%d %H:%M:%S")    [ORGINAL] $1 was backed up" >> /home/$USER/BACKME/log
 
 	fi
+#Elif File
 elif [ -f "$1" ] ; then
 	y=$(echo "$1" | rev | cut -d '.' -f 1 | rev)
 	echo""
@@ -97,18 +93,20 @@ elif [ -f "$1" ] ; then
 			fi
 	fi
 		
-
+#Else if no file available
 else
 	echo""
 	echo "[ERROR] !!! Something went wrong, nothing to backup !!!"
 	echo "|" >> /home/$USER/BACKME/log
 	echo "|___$(date "+%y/%m/%d %H:%M:%S")    [ERROR] Something funky happened, nothing was backed up                                                 [ERROR]" >> /home/$USER/BACKME/log
 fi
-d=$(du -sh /home/kim/BACKME/ | awk -F'\t' '{print $1}')
+#Log Disk space usage
+d=$(du -sh /home/$USER/BACKME/ | awk -F'\t' '{print $1}')
 echo "|_________$(date "+%y/%m/%d %H:%M:%S")    [DISKSPACE] The BACKME/ folder is currently $d in size" >> /home/$USER/BACKME/log
 echo""
 echo "[CURRENTLY USED DISKPACE]"
 echo "$(du -hs /home/$USER/BACKME/* | awk -F'\t' '{print $1 " " $2;}')"
 echo""
+#Call you a captain when all done
 echo "[END] All done captain!"
 echo""
