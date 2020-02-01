f2e21a950ff2df8f6847a913a87c5ebc2fc70575
wesipls <43371084+wesipls@users.noreply.github.com>
2020-02-01 15:03:42 +0200

    edited comment

--- certificates/bash07.sh
+++ certificates/bash07.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#7th excersize in udemy course, checks if /etc/shadow file exists using exit codes, exit code 0 stands for succesfull run (file exists) 1-255 stands for error.
+#7th excersize in udemy course, checks if /etc/shadow file exists using exit codes.
+#exit code 0 stands for succesfull run (file exists) 1-255 stands for error (file does not exist).
 
 cat /etc/shadow
 
