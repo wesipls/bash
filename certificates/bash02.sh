2d2b3757f32aa90bf908e1b9e08e0802aa67d5fc
wesipls <43371084+wesipls@users.noreply.github.com>
2020-02-01 14:55:46 +0200

    Added comments

--- certificates/bash02.sh
+++ certificates/bash02.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#Second excersize, checks if shadow passwords are enabled
 
 if [ -e "/etc/shadow" ]
 then
 	echo "Shadow passwords are enabled"
+#Checks user permission to edit the /etc/shadow file
 	if [ -w "/etc/shadow" ]
 	then 
 		echo "You have permission to edit /etc/shadow."
