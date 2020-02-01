b99b7bef2271770f67525f15800287c38895b693
Valkama <vesipls@vesipls.bb.dnainternet.fi>
2020-02-01 14:51:00 +0200

    Made script readeable for humans, added comments

--- certificates/bash01.sh
+++ certificates/bash01.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#REMEMBER TO CHMOD 755 .scripname.sh
+
+#First lesson of https://www.udemy.com/course/bash-scripting/
 
 echo "Shell Scripting is Fun!"
 
-VAR1="Shell Scripting is Fun!"
-
+#Check host and echo it to the shell
 HOST=$(hostname)
-
 echo "This script is running on ${HOST}. where "${HOST}" is the output of the "hostname" command"
