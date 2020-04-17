89af00fae4e0e03014ab134f6490fe82c8fa94b8
vesipls <vesipls@DebBox.bb.dnainternet.fi>
2020-04-17 15:43:06 +0300

    not sure why its changed, works now

--- personal_projects/postsleeptouchpad.sh
+++ personal_projects/postsleeptouchpad.sh
@@ -2,7 +2,7 @@
 #Script to start touchpadstart.sh that will re-enable touchpad.
 #Files located according to debian busters file locations
 case $1 in
-  post)
- /usr/local/bin/touchpadstart.sh
-  ;;
+  	post)
+ 	/usr/local/bin/touchpadstart.sh
+  	;;
 esac
