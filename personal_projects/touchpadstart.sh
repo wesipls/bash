22046287a6f2a3c6f4e75df93409f97036001a3b
vesipls <vesipls@DebBox.bb.dnainternet.fi>
2020-04-17 14:22:55 +0300

    Added comments

--- personal_projects/touchpadstart.sh
+++ personal_projects/touchpadstart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Short script to reset touchpad after sleeping
+#Folder structure according to debian buster
+#Script is started by another script, found below named postsleeptouchpad.sh
 
 /sbin/modprobe -r psmouse & wait
 /sbin/modprobe psmouse & wait
