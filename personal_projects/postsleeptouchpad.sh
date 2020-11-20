903b16ccb242b854a86ca845cb72d1b952df680e
wesipls <43371084+wesipls@users.noreply.github.com>
2020-11-20 15:27:30 +0200

    Delete postsleeptouchpad.sh

--- personal_projects/postsleeptouchpad.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#Script to start touchpadstart.sh that will re-enable touchpad.
-#Files located according to debian busters file locations
-case $1 in
-  	post)
- 	/usr/local/bin/touchpadstart.sh
-  	;;
-esac
