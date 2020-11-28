d83cd61903dd53b7d31e4db74cf131a73c1cf0d9
vesipls <vesipls@Debbox.bb.dnainternet.fi>
2020-11-28 20:36:20 +0200

    Created init script for servers

---
+++ personal_projects/server-init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Init script for new laptops, only works for debian deriatives
+
+#Check if script is run as superuser
+if [ "$EUID" -ne 0 ]
+then 
+	echo "This script needs to be run as root"
+	exit 1
+fi
+
+#Adding docker repository
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+#Updating and upgrading packages
+apt-get update
+apt-get upgrade -y
+
+#Add package to this list to install
+Packages=(apt-transport-https ca-certificates gnupg-agent software-properties wget curl git neovim firefox net-tools openssh-server docker-ce docker-ce-cli containered.io)
+for i in $Packages
+do
+	apt-get install $i -y
+done
+
+#Start and enable ssh server
+systemctl start ssh
+systemctl enable ssh
+
