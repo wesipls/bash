a11eb8a7a38a3ac7a49abd7b0d79c75851344c9b
vesipls <vesipls@Sora.bb.dnainternet.fi>
2021-02-06 19:39:36 +0200

    Fixed i3-gaps install script

--- install-i3-gaps.sh
+++ install-i3-gaps.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #Short script to install i3-gaps
 
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps/
-meson build
-ninja -C build/ install
+#Ninja build broken for some reason, using existing github project, thank you  maestrogerardo 
+#git clone https://www.github.com/Airblader/i3 i3-gaps
+#cd i3-gaps/
+#meson build
+#ninja -C build/ install
+
+
+git clone https://github.com/maestrogerardo/i3-gaps-deb.git
+cd i3-gaps-deb/
+./i3-gaps-deb 
+
